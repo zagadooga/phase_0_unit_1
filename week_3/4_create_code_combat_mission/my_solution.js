77af520f659fcbe18fdf2e2f92a2147b7a8757d2
zagadooga <ashford91@gmail.com>
2014-04-20 22:18:48 -0700

    Update my_solution.js

--- week_3/4_create_code_combat_mission/my_solution.js
+++ week_3/4_create_code_combat_mission/my_solution.js
@@ -68,7 +68,36 @@ var pigeon = {
 // 
 // 
 // 
-// 
+// What parts of your strategy worked? What problems did you face?
+
+//My partner and I were pretty effective at this challenge, partly because we planned out our code before we even began.
+//We had a good idea of what we wanted our product to be both in english and pseudocode. The only real problems we had
+//were not in the concept, but in the implementation; neither of us were experts in javascript so we had to pause and
+//check out online resources to solve the problems.
+
+//What questions did you have while coding? What resources did you find to help you answer them?
+
+//We googled a couple problems, I believe I used the mozilla developer network for one issue.
+
+//What concepts are you having trouble with, or did you just figure something out? If so, what?
+
+//Conceptually, the challenge was pretty straightforward; the problems were in the limitations of our javascript knowledge.
+
+//Did you learn any new skills or tricks?
+
+//Of course, I learned how to implement a function within an object in javascript.
+
+//How confident are you with each of the learning objectives?
+
+//Confident.
+
+//Which parts of the challenge did you enjoy?
+
+//I enjoyed the problem solving aspects, and pretty much every part before debugging.
+
+//Which parts of the challenge did you find tedious?
+
+//Debugging...obviously. It took a fair amount of time tinkering with the code to get it right after we had written it all out.
 // 
 // 
 // 
